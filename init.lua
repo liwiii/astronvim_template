3fc5b814d2a241a229860f72c8e84ad8ba3dbf7f
liwiii <liwei190315@gmail.com>
2024-07-25 11:02:37 +0800

    customize by liwiii

--- init.lua
+++ init.lua
@@ -17,3 +17,27 @@ end
 
 require "lazy_setup"
 require "polish"
+
+vim.cmd([[
+  inoremap <expr> <cr> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+  inoremap <silent><expr> <cr> coc#pum#visible() ? coc#_select_confirm() : "\<C-g>u\<CR>"
+  inoremap <silent><expr> <cr> coc#pum#visible() && coc#pum#info()['index'] != -1 ? coc#pum#confirm() : "\<C-g>u\<CR>"
+  " use <tab> to trigger completion and navigate to the next complete item
+  function! CheckBackspace() abort
+    let col = col('.') - 1
+    return !col || getline('.')[col - 1]  =~# '\s'
+  endfunction
+
+  inoremap <silent><expr> <Tab>
+        \ coc#pum#visible() ? coc#pum#next(1) :
+        \ CheckBackspace() ? "\<Tab>" :
+        \ coc#refresh()
+  " use <c-space> for trigger completion
+  inoremap <silent><expr> <c-space> coc#refresh()
+  " Use <C-@> on vim
+  inoremap <silent><expr> <c-@> coc#refresh()
+  inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+  inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+]])
+
+
